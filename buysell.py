ab967410c6b41406e38878677135197d15062c62
Nihal <nihal@Nihals-MacBook-Air.local>
2021-10-17 16:50:24 +0530

    WIP: Ads connected to data base

--- buysell.py
+++ buysell.py
@@ -9,14 +9,16 @@ def fetchEnumTable(tableName,columns ):
 	conn = sqlite3.connect('buzzDatabase.db')
 	cur = conn.cursor()
 	query = "select "+columns+" from "+tableName
+	print("\n\n\n\n"+query)
 	data  = cur.execute(query).fetchall()
 	print(data)
 	return data
 
 @app.route('/')
 def home():
-
-   return render_template('buysell.html')
+	categoty = fetchEnumTable("categories","*")
+	data = fetchEnumTable("products","title,bid_inc,product_availability,updated_at")	
+	return render_template('buysell.html',products=data)
 
 if __name__ == '__main__':
 	app.run(debug=True)
